9755c66ae7f19c8f6975f75099faf6450ffbcbc3
Carlos Machado <124357036+camachado64@users.noreply.github.com>
2025-09-19 00:53:47 +0200

    Update db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -12,7 +12,7 @@ export const dbConnection: ConnectionPool = new ConnectionPool({
   password: config.dbPassword,
   database: config.dbName,
   options: {
-    encrypt: false,
+    encrypt: true,
     trustServerCertificate: true,
     enableArithAbort: true,
   },
